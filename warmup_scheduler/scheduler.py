d649ee29c9c96440f23a054a6399e46c84e8d2d8
curtis.abcd <curtis.abcd@kakaobrain.com>
2019-01-17 11:34:40 +0900

    bug fix : add step function call

--- warmup_scheduler/scheduler.py
+++ warmup_scheduler/scheduler.py
@@ -23,7 +23,16 @@ class GradualWarmupScheduler(_LRScheduler):
     def get_lr(self):
         if self.last_epoch > self.total_epoch:
             if self.after_scheduler:
+                if not self.finished:
+                    self.after_scheduler.base_lrs = [base_lr * self.multiplier for base_lr in self.base_lrs]
+                    self.finished = True
                 return self.after_scheduler.get_lr()
             return [base_lr * self.multiplier for base_lr in self.base_lrs]
 
         return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in self.base_lrs]
+
+    def step(self, epoch=None):
+        if self.finished and self.after_scheduler:
+            return self.after_scheduler.step(epoch)
+        else:
+            return super(GradualWarmupScheduler, self).step(epoch)
