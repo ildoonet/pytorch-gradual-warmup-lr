0ecaf0d389988a6083dcf66b2fe236c93df73451
curtis.abcd <curtis.abcd@kakaobrain.com>
2019-01-18 11:23:44 +0900

    hot fix

--- warmup_scheduler/scheduler.py
+++ warmup_scheduler/scheduler.py
@@ -9,7 +9,7 @@ class GradualWarmupScheduler(_LRScheduler):
         optimizer (Optimizer): Wrapped optimizer.
         multiplier: target learning rate = base lr * multiplier
         total_epoch: target learning rate is reached at total_epoch, gradually
-        after_schduler: after target_epoch, use this scheduler(eg. ReduceLROnPlateau)
+        after_scheduler: after target_epoch, use this scheduler(eg. ReduceLROnPlateau)
     """
 
     def __init__(self, optimizer, multiplier, total_epoch, after_scheduler=None):
@@ -18,6 +18,7 @@ class GradualWarmupScheduler(_LRScheduler):
             raise ValueError('multiplier should be greater than 1.')
         self.total_epoch = total_epoch
         self.after_scheduler = after_scheduler
+        self.finished = False
         super().__init__(optimizer)
 
     def get_lr(self):
