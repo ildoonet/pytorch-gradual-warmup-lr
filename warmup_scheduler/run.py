b1ae2943ef42c1168dc95ad523f57269fcbb5d18
curtis.abcd <curtis.abcd@kakaobrain.com>
2020-04-04 14:09:58 +0900

    update example without warning message.

--- warmup_scheduler/run.py
+++ warmup_scheduler/run.py
@@ -9,9 +9,13 @@ if __name__ == '__main__':
     model = [torch.nn.Parameter(torch.randn(2, 2, requires_grad=True))]
     optim = SGD(model, 0.1)
 
+    # scheduler_warmup is chained with schduler_steplr
     scheduler_steplr = StepLR(optim, step_size=10, gamma=0.1)
     scheduler_warmup = GradualWarmupScheduler(optim, multiplier=1, total_epoch=5, after_scheduler=scheduler_steplr)
-    # scheduler_warmup is chained with schduler_steplr
+
+    # this zero gradient update is needed to avoid an warning message, issue #8.
+    optim.zero_grad()
+    optim.step()
 
     for epoch in range(1, 20):
         scheduler_warmup.step(epoch)
